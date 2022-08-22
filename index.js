ed4bffb90757c4b829ecc1c9e475703e62422cf1
Tanner Hawkins <TTHawkins@student.fullsail.edu>
2022-08-22 16:32:36 -0400

    deployment test

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/moo', (request, response, next) => {
+  response.json("moo")
+});
+
+// If any request doesn't match one of the above endpoints, send React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`App listening on port ${port}...`);
+
